b8777858aae70da7f1d15b369b61275e7c009416
Enoch2k2 <enoch2k2@hotmail.com>
2017-02-13 13:49:13 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      return Object.assign({}, state, {
+        friends: [...state.friends, action.friend]
+      });
+    case 'REMOVE_FRIEND':
+      var friends = [...state.friends];
+      var i = 0;
+      friends.forEach((friend, index) => {
+        if(friend.id === action.id){
+          i = index;
+          return;
+        }
+      });
+      friends.splice(i, 1);
+      return Object.assign({}, state, {
+        friends: friends
+      });
+    default:
+      return state;
+  }
 }
